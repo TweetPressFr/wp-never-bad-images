edb2e715c0d5a5e60385d1073712fc9e95bca0c3
Julien Maury <6878027+jmau111@users.noreply.github.com>
2021-04-16 16:21:08 +0200

    fix indentation

--- wp-never-bad-images.php
+++ wp-never-bad-images.php
@@ -1,38 +1,39 @@
 <?php
+
 /**
  * Plugin name: WP Never Bad Images
  * Author: Julien Maury
  * Author URI: https://www.julien-maury.dev
- * Version: 0.111
+ * Version: 0.2
  * Description: I don't encourage the use of this plugin in production. It's quite experimental.
  */
 
-defined( 'DB_USER' )
-	or die;
+defined('DB_USER')
+    or die;
 
-define( 'WP_NBI_DIR', plugin_dir_path( __FILE__ ) );
-define( 'WP_NBI_DEFAULT_IMG', 'http://placehold.it/300?text=OOPS+IMAGE+MISSING' );
+define('WP_NBI_DIR', plugin_dir_path(__FILE__));
+define('WP_NBI_DEFAULT_IMG', 'http://placehold.it/300?text=OOPS+IMAGE+MISSING');
 
-add_filter( 'the_content', function( $content ) {
+add_filter('the_content', function ($content) {
 
-	if ( ! class_exists( 'domDocument' ) ) {
-		return $content;
-	}
+    if (!class_exists('domDocument')) {
+        return $content;
+    }
 
-	$dom  = new domDocument;
-	$dom->loadHTML($content);
-	$dom->preserveWhiteSpace = false;
-	$images = $dom->getElementsByTagName('img');
+    $dom  = new domDocument;
+    $dom->loadHTML($content);
+    $dom->preserveWhiteSpace = false;
+    $images = $dom->getElementsByTagName('img');
 
-	$default = esc_url( apply_filters( 'wp_nbi_default_img_url', WP_NBI_DEFAULT_IMG, $content ) );
+    $default = esc_url(apply_filters('wp_nbi_default_img_url', WP_NBI_DEFAULT_IMG, $content));
 
-	foreach ( $images as $image ) {
+    foreach ($images as $image) {
 
-		$src = $image->getAttribute('src');
-		if ( ! @getimagesize( $src ) ) { // we all love @, don't we ?
-			$content = str_replace( $src, $default, $content );
-		}
-	}
+        $src = $image->getAttribute('src');
+        if (!@getimagesize($src)) { // we all love @, don't we ?
+            $content = str_replace($src, $default, $content);
+        }
+    }
 
-	return $content;
+    return $content;
 });
